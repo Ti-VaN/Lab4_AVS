599201dc09bf5978e48b2b40fc83ab2cd4b835d2
Funny-Code-d <sosnin_dienis@mail.ru>
2020-12-07 16:08:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,13 +11,15 @@ do
 		#Запуск отлаженой готовой программы
 		1)
 			#Проверка существования файла
+			echo "Enter parameters:"
+			read param
 			if [ -e $PWD/old_version.exe ]
 			then
-				./old_version.exe
+				./old_version.exe $param
 			else
 				if [ -e $PWD/new_version.exe ]
 				then	
-					./new_version.exe
+					./new_version.exe $param
 				#Если ни один файл не найден
 				else
 					echo "File not found"
@@ -28,25 +30,29 @@ do
 		2)
 			echo "Enter name file.cpp"
 			read file
+			echo "Enter parameters:"
+			read param
 			if [ -e $PWD/new_version.exe ]
 			then
 				#Если есть уже отлаженая программа
 				#меняется её название, компилируется новая
 				mv new_version.exe old_version.exe
 				g++ -o new_version.exe $file
-				./new_version.exe
+				./new_version.exe $param
 			else
 				#если первая компиляция
 				g++ -o new_version.exe $file
-				./new_version.exe
+				./new_version.exe $param
 			fi
 			;;
 		3)
 			#Перезапись предидущей компиляции
 			echo "Enter name file.cpp"
 			read file
+			echo "Enter parameters:"
+			read param
 			g++ -o new_version.exe $file
-			./new_version.exe
+			./new_version.exe $param
 			;;
 		4)
 			exit 0
